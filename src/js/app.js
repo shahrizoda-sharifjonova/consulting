7d9e949a49b84708042cb5ab55075f3775e0caf4
shahrizoda <shahrizodadeveloping@gmail.com>
2022-07-17 19:47:58 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -7,44 +7,63 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 
 new Swiper(".intro__swiper", {
-    modules: [Pagination, Navigation],
-    slidesPerView: 1,
-    autoplay:{
-        delay: 2000,
-    },
-    pagination:{
-        el: ".intro__pagination",
-        type: "fraction",
-    },
-    navigation: {
-      nextEl: ".intro__button-next",
-      prevEl: ".intro__button-prev",
-    },
-  });
+  modules: [Pagination, Navigation],
+  slidesPerView: 1,
+  autoplay:{
+      delay: 2000,
+  },
+  pagination:{
+      el: ".intro__pagination",
+      type: "fraction",
+  },
+  navigation: {
+    nextEl: ".intro__button-next",
+    prevEl: ".intro__button-prev",
+  },
+});
 
 
 new Swiper(".clients__swiper", {
-    modules: [Pagination, Navigation],
-    slidesPerView: 2,
-    autoplay:{
-        delay: 2000,
+  modules: [Pagination, Navigation],
+  slidesPerView: 2,
+  autoplay:{
+      delay: 2000,
+  },
+  pagination:{
+      el: ".clients__pagination",
+  },
+  navigation: {
+    nextEl: ".clients__button-next",
+    prevEl: ".clients__button-prev",
+  },
+  breakpoints:{
+    1200:{
+      slidesPerView: 4,
     },
-    pagination:{
-        el: ".clients__pagination",
-    },
-    navigation: {
-      nextEl: ".clients__button-next",
-      prevEl: ".clients__button-prev",
-    },
-    breakpoints:{
-      1200:{
-        slidesPerView: 4,
-      },
-      768:{
-        slidesPerView: 3,
-      }
+    768:{
+      slidesPerView: 3,
     }
-  });
+  }
+});
+
+
+new Swiper(".news__swiper", {
+  modules: [Pagination, Navigation],
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination:{
+      el: ".news__pagination",
+  },
+  navigation: {
+    nextEl: ".news__button-next",
+    prevEl: ".news__button-prev",
+  },
+  breakpoints:{
+    768:{
+      slidesPerView: 2,
+    }
+  }
+});
 
 const menu = document.querySelector('.menu');
 const headerList = document.querySelector('.header__list');
