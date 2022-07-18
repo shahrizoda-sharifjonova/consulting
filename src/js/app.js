1b2bd74f51e5801672116c2606050b98ef65e07a
shahrizoda <shahrizodadeveloping@gmail.com>
2022-07-18 14:20:47 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -67,6 +67,30 @@ new Swiper(".news__swiper", {
 });
 
 
+new Swiper(".trust__swiper", {
+  modules: [Pagination, Navigation],
+  slidesPerView: 2,
+  autoplay:{
+      delay: 2000,
+  },
+  pagination:{
+      el: ".trust__pagination",
+  },
+  navigation: {
+    nextEl: ".trust__button-next",
+    prevEl: ".trust__button-prev",
+  },
+  breakpoints:{
+    1200:{
+      slidesPerView: 4,
+    },
+    768:{
+      slidesPerView: 3,
+    }
+  }
+});
+
+
 new Swiper(".new__swiper", {
   modules: [Pagination],
   slidesPerView: 1,
