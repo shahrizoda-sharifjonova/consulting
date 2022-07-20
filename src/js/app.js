81707a2bb87c0fad085b38a8d3e638eb3f2435b4
shahrizoda <shahrizodadeveloping@gmail.com>
2022-07-20 11:12:53 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -153,3 +153,21 @@ menu.addEventListener('click', (e)=>{
     menu.classList.toggle('active')
     body.classList.toggle('hidden')
 })
+
+$(document).ready(function() {
+  $(".accordion > .accordion__button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".accordion__content")
+        .slideUp(200);
+    } else {
+      $(".accordion > .accordion__button").removeClass("active");
+      $(this).addClass("active");
+      $(".accordion__content").slideUp(200);
+      $(this)
+        .siblings(".accordion__content")
+        .slideDown(200);
+    }
+  });
+});
